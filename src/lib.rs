97aa5dbc6caca32484a3e61f3c8a8c063462fb1f
MatthD <matthias.dieudonne@gmail.com>
2023-09-12 17:47:13 +0200

    feat(xmltree) working wellformed

--- src/lib.rs
+++ src/lib.rs
@@ -3,19 +3,11 @@ extern crate xmltree;
 use std::fs::File;
 use std::io::Read;
 
+
 use xmltree::Element;
 
-fn main() {
-    // Nom du fichier XML à lire
-    let filename = "tests/data/test-not-wellformed.xml";
 
-    if let Some(root) = xml_read_file(filename){
-        // Par exemple, afficher le nom de la balise racine
-        println!("Balise racine : {}", root.name);
-    }
-}
-
-fn xml_read_file(path: &str) -> Option<Element> {
+pub fn xml_read_file(path: &str) -> Result<Element, String> {
     dbg!(&path);
     // Open the XML file
     let mut file = File::open(path).expect("Error opening XML file.");
@@ -25,7 +17,7 @@ fn xml_read_file(path: &str) -> Option<Element> {
     file.read_to_string(&mut xml_string).expect("Error reading XML file.");
 
     // Parse the XML string
-    Some(Element::parse(xml_string.as_bytes()).expect("Error parsing XML."))
+    Ok(Element::parse(xml_string.as_bytes()).expect("Error parsing XML."))
 }
 
 
@@ -39,7 +31,7 @@ mod tests {
 
         // Read the XML file using the xml_read_file function
         // We dont cover the case the file does not exist and return none its another test..
-        if let Some(root_element) = xml_read_file("./tests/data/test-default.xml"){
+        if let Ok(root_element) = xml_read_file("./tests/data/test-default.xml"){
             // Check that the root element is correct
             assert_eq!(root_element.name, "xpath");
             let infosdust = root_element.get_child("to").unwrap().get_child("my").unwrap().get_child("infos").unwrap().get_text().unwrap();
@@ -53,12 +45,16 @@ mod tests {
     #[should_panic(expected = "Error opening XML file.")]
     fn test_xml_read_file_not_existing_file() {
         // Try to read a not wellformed XML file that does not exist
-        xml_read_file("tests/data/non_existent_file.xml");
+        if let Err(err) = xml_read_file("tests/data/non_existent_file.xml") {
+            panic!("{}", err);
+        };
     }
     #[test]
     #[should_panic(expected = "Error parsing XML.")]
     fn test_xml_read_file_not_wellformed_file() {
         // Try to read a not wellformed XML file that does not exist
-        xml_read_file("tests/data/test-not-wellformed.xml");
+        if let Err(err)  = xml_read_file("tests/data/test-not-wellformed.xml") {
+            panic!("{}", err);
+        }
     }
 }
